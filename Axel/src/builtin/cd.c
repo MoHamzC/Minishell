d8ea755aa4e60ae7c43e197a4db96f1cdd16e317
Axel Burin Des Roziers <axburin-@c2r4p6.42lehavre.fr>
2025-02-13 18:33:09 +0100

    AXEL

--- Axel/src/builtin/cd.c
+++ Axel/src/builtin/cd.c
@@ -6,14 +6,52 @@
 /*   By: axburin- <axburin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 16:56:26 by axburin-          #+#    #+#             */
-/*   Updated: 2025/02/12 09:18:31 by axburin-         ###   ########.fr       */
+/*   Updated: 2025/02/13 18:19:13 by axburin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-ft_();
+void ft_update_env(t_env *envp, const char *key, const char new_value)
+{
+	while(envp->next)
+	{
+		if(strncmp(envp->key, key, strlen(key)) == 0)
+		{
+			free(envp->key);
+			envp->value = ft_strdup(&new_value);
+			return;
+		}
+		envp = envp->next;
+	}
+}
+void ft_cd(t_env *envp)
+{
+	char *path;
+	char *cwd;
+	char *home;
 
-ft_two_args();
-
-ft_cd();
+	path = NULL;
+	cwd = NULL;
+	home = NULL;
+	if(!path)
+	{
+		home = getenv("HOME");
+		if(home == NULL)
+		{
+			write(2, "HOME not set\n", 14);
+			return ;
+		}
+		path = home;
+	}
+	if(chdir(path) == -1)
+	{
+		perror("cd");
+		return;
+	}
+	printf("%s\n", path);
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
+		ft_update_env(envp, "PWD", *cwd);
+	if (getenv("PWD"))
+		ft_update_env(envp, "OLDPWD", *getenv("PWD"));
+}
