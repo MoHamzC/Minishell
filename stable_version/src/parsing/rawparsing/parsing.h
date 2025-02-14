c787a6683a0069ae80deed91cafe291853757253
marionelstrt <mariontarento@gmail.com>
2025-02-14 02:52:32 +0100

    norminette for parsing.h

--- stable_version/src/parsing/rawparsing/parsing.h
+++ stable_version/src/parsing/rawparsing/parsing.h
@@ -6,7 +6,7 @@
 /*   By: mtarento <mtarento@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 04:33:59 by mochamsa          #+#    #+#             */
-/*   Updated: 2025/02/14 02:51:37 by mtarento         ###   ########.fr       */
+/*   Updated: 2025/02/14 02:52:17 by mtarento         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 # define PARSING_H
 # include "minishell.h"
 
-int ft_strlen_tab(char **tab);
-t_command *new_command(void); 
-void	add_argument(t_command *cmd, char *arg); 
-void	add_redirection(t_command *cmd, t_token *token); 
-int	is_rdir_tok(t_cmd_type type);
-void init_parse(t_command **cmds, t_command **current); 
+int			ft_strlen_tab(char **tab);
+t_command	*new_command(void); 
+void		add_argument(t_command *cmd, char *arg); 
+void		add_redirection(t_command *cmd, t_token *token); 
+int			is_rdir_tok(t_cmd_type type);
+void		init_parse(t_command **cmds, t_command **current); 
 t_command	*parse_tokens(t_token **tokens); 
-int whichbuiltin(char *s);
+int			whichbuiltin(char *s);
 
 #endif // PARSING_H
