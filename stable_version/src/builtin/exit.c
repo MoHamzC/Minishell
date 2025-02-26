176ac32ecd68df7c9d507a01bc92a4aa75478199
Axel Burin Des Roziers <axburin-@c1r7p9.42lehavre.fr>
2025-02-26 21:48:30 +0100

    norm_builtins & debut exec.c

--- stable_version/src/builtin/exit.c
+++ stable_version/src/builtin/exit.c
@@ -3,65 +3,65 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: calberti <calberti@student.42.fr>          +#+  +:+       +#+        */
+/*   By: axburin- <axburin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 19:12:54 by axburin-          #+#    #+#             */
-/*   Updated: 2025/02/26 21:16:27 by calberti         ###   ########.fr       */
+/*   Updated: 2025/02/26 21:22:15 by axburin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void    exit_free(t_shell *shell, t_exec_data *exec)
+static void	exit_free(t_shell *shell, t_exec_data *exec)
 {
-    free_env_array(&exec->env_arr);
-    restore_std_fds(exec);
-    ft_free_commands(&shell->cmds);
-    free_env(shell->env);
-    clean_heredoc_f(shell->here_docs);
+	free_env_array(&exec->env_arr);
+	restore_std_fds(exec);
+	ft_free_commands(&shell->cmds);
+	free_env(shell->env);
+	clean_heredoc_f(shell->here_docs);
 }
 
-bool    ft_is_numeric(char *cmd)
+bool	ft_is_numeric(char *cmd)
 {
-    int    i;
+	int	i;
 
-    i = 0;
-    if (cmd[i] == '-' || cmd[i] == '+')
-        i++;
-    while (cmd[i])
-    {
-        if (!ft_isdigit(cmd[i]))
-            return (false);
-        i++;
-    }
-    return (true);
+	i = 0;
+	if (cmd[i] == '-' || cmd[i] == '+')
+		i++;
+	while (cmd[i])
+	{
+		if (!ft_isdigit(cmd[i]))
+			return (false);
+		i++;
+	}
+	return (true);
 }
 
-int    ft_exit(int argc, char **argv, t_shell *shell, t_exec_data *exec)
+int	ft_exit(int argc, char **argv, t_shell *shell, t_exec_data *exec)
 {
-    int    exit_status;
+	int	exit_status;
 
-    exit_status = 0;
-    if (argc > 2)
-    {
-        ft_putstr_fd("too many arguments\n", 2);
-        shell->exit_status = 1;
-        return (-1);
-    }
-    if (argc == 2)
-    {
-        if (!ft_is_numeric(argv[1]))
-        {
-            ft_putstr_fd("numeric argument required\n", 2);
-            exit_status = 2;
-            (exit_free(shell, exec), (exit(exit_status)));
-        }
-        exit_status = ft_atoi(argv[1]);
-    }
-    if (exit_status < 0)
-        exit_status += 256;
-    exit_status %= 256;
-    printf("exit\n");
-    exit_free(shell, exec);
-    exit(exit_status);
+	exit_status = 0;
+	if (argc > 2)
+	{
+		ft_putstr_fd("too many arguments\n", 2);
+		shell->exit_status = 1;
+		return (-1);
+	}
+	if (argc == 2)
+	{
+		if (!ft_is_numeric(argv[1]))
+		{
+			ft_putstr_fd("numeric argument required\n", 2);
+			exit_status = 2;
+			(exit_free(shell, exec), (exit(exit_status)));
+		}
+		exit_status = ft_atoi(argv[1]);
+	}
+	if (exit_status < 0)
+		exit_status += 256;
+	exit_status %= 256;
+	printf("exit\n");
+	exit_free(shell, exec);
+	exit(exit_status);
 }
--- stable_version/src/builtin/export.c
+++ stable_version/src/builtin/export.c
@@ -3,86 +3,86 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: calberti <calberti@student.42.fr>          +#+  +:+       +#+        */
+/*   By: axburin- <axburin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 20:45:23 by axburin-          #+#    #+#             */
-/*   Updated: 2025/02/26 21:15:57 by calberti         ###   ########.fr       */
+/*   Updated: 2025/02/26 21:28:43 by axburin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void    error_and_free(char *key, char *value)
+static void	error_and_free(char *key, char *value)
 {
-    ft_putstr_fd("export: `", 2);
-    ft_putstr_fd(key, 2);
-    ft_putstr_fd("': not a valid identifier\n", 2);
-    free(key);
-    free(value);
+	ft_putstr_fd("export: `", 2);
+	ft_putstr_fd(key, 2);
+	ft_putstr_fd("': not a valid identifier\n", 2);
+	free(key);
+	free(value);
 }
 
-bool    ift_is_valid_env_var(char *key)
+bool	ift_is_valid_env_var(char *key)
 {
-    int    i;
+	int	i;
 
-    if ((!key || !ft_isalpha(key[0])) && (key[0] != '_'))
-        return (false);
-    i = 1;
-    while (key[i] != '\0')
-    {
-        if (!ft_isalnum(key[i]) && key[i] != '_')
-            return (false);
-        i++;
-    }
-    return (true);
+	if ((!key || !ft_isalpha(key[0])) && (key[0] != '_'))
+		return (false);
+	i = 1;
+	while (key[i] != '\0')
+	{
+		if (!ft_isalnum(key[i]) && key[i] != '_')
+			return (false);
+		i++;
+	}
+	return (true);
 }
 
-void    push_env_var(t_shell *shell, char *key, char *value)
+void	push_env_var(t_shell *shell, char *key, char *value)
 {
-    t_env    *env;
-    t_env    *new_env;
+	t_env	*env;
+	t_env	*new_env;
 
-    env = shell->env;
-    while (env)
-    {
-        if (ft_strcmp(env->key, key) == 0)
-        {
-            free(env->value);
-            env->value = ft_strdup(value);
-            return ;
-        }
-        env = env->next;
-    }
-    new_env = malloc(sizeof(t_env));
-    new_env->key = ft_strdup(key);
-    new_env->value = ft_strdup(value);
-    new_env->next = shell->env;
-    shell->env = new_env;
+	env = shell->env;
+	while (env)
+	{
+		if (ft_strcmp(env->key, key) == 0)
+		{
+			free(env->value);
+			env->value = ft_strdup(value);
+			return ;
+		}
+		env = env->next;
+	}
+	new_env = malloc(sizeof(t_env));
+	new_env->key = ft_strdup(key);
+	new_env->value = ft_strdup(value);
+	new_env->next = shell->env;
+	shell->env = new_env;
 }
 
-int    ft_export(char **args, t_shell *shell)
+int	ft_export(char **args, t_shell *shell)
 {
-    char    *key;
-    char    *value;
-    int        i;
-    int        j;
+	char	*key;
+	char	*value;
+	int		i;
+	int		j;
 
-    i = 1;
-    while (args[i])
-    {
-        j = 0;
-        while (args[i][j] && args[i][j] != '=')
-            j++;
-        if (j == (int)ft_strlen(args[i]))
-            return (1);
-        key = ft_substr(args[i], 0, j);
-        value = ft_substr(args[i], j + 1, ft_strlen(args[i]) - j - 1);
-        if (ift_is_valid_env_var(key))
-            push_env_var(shell, key, value);
-        else
-            return(error_and_free(key, value), 1);
-        (free(key), free(value));
-        i++;
-    }
-    return (0);
+	i = 1;
+	while (args[i])
+	{
+		j = 0;
+		while (args[i][j] && args[i][j] != '=')
+			j++;
+		if (j == (int)ft_strlen(args[i]))
+			return (1);
+		key = ft_substr(args[i], 0, j);
+		value = ft_substr(args[i], j + 1, ft_strlen(args[i]) - j - 1);
+		if (ift_is_valid_env_var(key))
+			push_env_var(shell, key, value);
+		else
+			return (error_and_free(key, value), 1);
+		(free(key), free(value));
+		i++;
+	}
+	return (0);
 }
